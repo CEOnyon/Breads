f2b559da717dfe4befd9925b163b20fa64b3e6b2
Courtney Hatcher <courtneyehatcher@gmail.com>
2022-09-15 19:54:01 -0700

    sending index and show data

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express()
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
 // Routes
 app.get('/', (req, res) => {
@@ -17,8 +18,12 @@ app.get('/', (req, res) => {
 // Breads
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
-  
 
-app.listen(PORT, () => {
+// 404 Page
+app.get('*', (req, res) => {
+    res.send('404')
+  })
+  
+  app.listen(PORT, () => {
     console.log('listening on port', PORT)
 })
